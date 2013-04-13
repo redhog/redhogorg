b27f563c758f2f870a46cf7580d3000e39fa2f74
Egil Moeller <egil.moller@freecode.no>
2013-04-14 01:35:48 +0200

    Bugfix for djangoadmin for field include_tags

--- appomatic_redhogorg_data/admin.py
+++ appomatic_redhogorg_data/admin.py
@@ -6,7 +6,7 @@ import fcdjangoutils.fields
 
 django.contrib.admin.site.register(appomatic_redhogorg_data.models.Tag, mptt.admin.MPTTModelAdmin)
 class NodeAdmin(autocomplete.widgets.AutocompleteModelAdmin):
-        related_search_fields = {'tags': ('name',)}
+        related_search_fields = {'tags': ('name',), 'include_tags': ('name',)}
 django.contrib.admin.site.register(appomatic_redhogorg_data.models.Category)
 django.contrib.admin.site.register(appomatic_redhogorg_data.models.Source)
 django.contrib.admin.site.register(appomatic_redhogorg_data.models.Article, NodeAdmin)
